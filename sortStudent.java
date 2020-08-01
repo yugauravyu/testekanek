6768206383514677da537ecdacbfa37db395ff1b
yugauravyu <gauravchauhan11.12@gmail.com>
2020-08-01 10:41:12 +0530

    Create sortStudent.java

---
+++ sortStudent.java
@@ -0,0 +1,37 @@
+Class Student {
+  String name;
+  int age;
+  int marks;
+  String rollNumber;
+} 
+
+
+class Main 
+{ 
+  //sort method 
+    public Collection<Student> sort(Collection<Student> students, String[] criteria){
+    Collections.sort(students, new Comparator<Student>() {  
+        @Override  
+        public int compare(Student s1, Student s2) { 
+            compareBu = CompareToBuilder();
+            for(i=0;i<criteria.length;i++){
+                compareBu.append(s1.criteria[i],s2.criteria[i)
+            }
+            return new compareBu.toComparison();  
+        }  
+      });  
+    }
+
+    public static void main (String[] args) 
+    { 
+        ArrayList<Student> ar = new ArrayList<Student>(); 
+        ar.add(new Student("akshay", 26, 34, 01)); 
+        ar.add(new Student("abhay", 36, 35, 04)); 
+        ar.add(new Student("akshay2", 36, 37, 03)); 
+    }
+    s1 = sort(ar, ["name"])
+    s2 = sort(ar, ["name","age"])
+}
+
+// time complexity N*log(N);
+// space complexity (N);
