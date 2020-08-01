9f595984d52c043a2eb54d381fba1c79383e0c93
yugauravyu <gauravchauhan11.12@gmail.com>
2020-08-01 10:51:38 +0530

    Update sortStudent.java

--- sortStudent.java
+++ sortStudent.java
@@ -1,13 +1,24 @@
-Class Student {
-  String name;
-  int age;
-  int marks;
-  String rollNumber;
-} 
 
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
 
-class Main 
+class Student {
+      String name;
+      int age;
+      int marks;
+      String rollNumber;
+      public Student(String name, String rollNumber, int age, int marks){
+          this.name = name;
+          this.age = age;
+          this.marks = marks;
+          this.rollNumber = rollNumber;
+      }
+    } 
+    
+public class Main 
 { 
+    
   //sort method 
     public Collection<Student> sort(Collection<Student> students, String[] criteria){
     Collections.sort(students, new Comparator<Student>() {  
@@ -15,7 +26,7 @@ class Main
         public int compare(Student s1, Student s2) { 
             compareBu = CompareToBuilder();
             for(i=0;i<criteria.length;i++){
-                compareBu.append(s1.criteria[i],s2.criteria[i)
+                compareBu.append(s1.criteria[i],s2.criteria[i]);
             }
             return new compareBu.toComparison();  
         }  
@@ -29,9 +40,6 @@ class Main
         ar.add(new Student("abhay", 36, 35, 04)); 
         ar.add(new Student("akshay2", 36, 37, 03)); 
     }
-    s1 = sort(ar, ["name"])
-    s2 = sort(ar, ["name","age"])
+    s1 = sort(ar, ["name"]);
+    s2 = sort(ar, ["name","age"]);
 }
-
-// time complexity N*log(N);
-// space complexity (N);
